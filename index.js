803db9e63d9cce1fb827fcae6ae83483df89ab98
valls <albert.valls.gonzalvo@gmail.com>
2015-06-16 20:26:24 +0200

    Fake db

--- index.js
+++ index.js
@@ -9,19 +9,28 @@ var app = express();
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+//Simulates database
+var db = {};
+
 //Routes
 //app.get('/', function(req, res){
 //  res.send('Hello world');
 //});
 
 app.post('/planets', function(req, res){
-  var nuevoPlaneta = req.body.planet;
-  //planet.id = "Hola";
+
+  console.log(JSON.stringify(db));
+
+  var newPlanet = req.body.planet;
+  newPlanet.id = new Date();
+
+  db[newPlanet.id] = newPlanet;
+
   res.set('Content-Type', 'application/json');
   res.status(201);
   res.send(planet);
 });
 
 app.listen(8000, function(){
-  console.log('Listening on port 3000...');
+  console.log('Listening on port 8000...');
 });
